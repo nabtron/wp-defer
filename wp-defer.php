d978f0c3ed4b418f5e0bf098aed692b341019439
nabtron <developer@nabtron.com>
2019-05-08 11:22:57 +0100

    Create wp-defer.php

---
+++ wp-defer.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: wp defer
+Description: defer plugin for wordpress.
+Version: 0.0.1
+Plugin URI: https://nabtron.com/
+Author: nabtron
+*/
+
+// include defere images
+include_once('defer_images.php');
